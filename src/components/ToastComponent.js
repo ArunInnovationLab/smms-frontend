13f385125eae5b69a0471f5d4c421b51682e4910
arun <you@example.com>
2023-12-10 21:30:39 +0530

    commit

---
+++ src/components/ToastComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+
+function ToastComponent() {
+  const notify = () => toast("Wow so easy!");
+
+  return (
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default ToastComponent;
